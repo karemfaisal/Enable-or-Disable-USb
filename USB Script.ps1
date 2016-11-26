0fed07a9e1f90daab52f5bc4f32721565fa443fb
Karem Ali <holyalo@yahoo.com>
2016-11-26 18:40:58 +0200

    Add files via upload

---
+++ USB Script.ps1
@@ -0,0 +1,47 @@
+﻿#Enable & Disable USB 
+#Part of IHACK Scripting Tutorial
+#Run It As Administrator 
+
+#Coded By Karem Ali
+
+If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(
+
+   [Security.Principal.WindowsBuiltInRole] “Administrator”))
+
+{
+
+ Write-Warning “You do not have Administrator rights to run this script!`nPlease re-run this script as an Administrator!”
+
+ Pause
+ Break
+
+}
+
+
+echo " Choose an Option " " 1- Disable USB " " 2- Enable USb "
+$A = read-host "Enter"
+$Path = "HKLM:\system\CurrentControlSet\Services\USBSTOR"
+if ( $A -eq 1 )
+{
+ if(test-path $Path)
+ {
+    Set-ItemProperty -path $Path -name start -value 4
+    echo " USB Has been Disabled "
+    pause
+ }
+}
+
+elseif ( $A -eq 2 )
+{
+if(test-path $Path)
+ {
+    Set-ItemProperty -path $path -name start -value 3
+     echo " USB Has been Enabled "
+    pause
+ }
+}
+else 
+{
+ echo " Choose 1 or 2 "
+    pause
+}
